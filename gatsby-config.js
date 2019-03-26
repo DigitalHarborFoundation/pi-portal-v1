2ade2362a31614af24069933c093f59c5657ffd7
jonathanprozzi <jonathanprozzi@gmail.com>
2019-03-26 15:52:17 -0400

    updated metadata in the config, tweaked global css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Digital Harbor Foundation Pi Portal`,
+    description: `Digital Harbor Foundation's Pi Portal project for the Pi Pilot project in Spring 2018.`,
+    author: `@jonathanprozzi`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#F5FCFF`,
+        theme_color: `#F5FCFF`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
