c102822db12d2a01018b3ba5d02e99607bb83f75
jonathanprozzi <jonathanprozzi@gmail.com>
2019-03-29 13:44:21 -0400

    added gatsby-mdx to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-mdx`,
     `gatsby-plugin-offline`,
   ],
 };
